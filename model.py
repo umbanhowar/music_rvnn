66478fda2a70872c4c32c4507c9afbc4177d6348
umbanhowar <nate.e.umb@gmail.com>
2017-12-11 10:54:02 -0500

    Add first draft of model encoder

--- model.py
+++ model.py
@@ -3,15 +3,59 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 
-escs = [torch.IntTensor(np.random.randint(2, size=[127, np.random.randint(4,10)])) 
-        for _ in xrange(4)]
 
 # HYPERPARAMETERS
 CODE_SIZE = 50
 EMBED_SIZE = 127
 
-# TODO: how to best handle encoding many-hot vectors? 
-embed = Variable(torch.randn(127, EMBED_SIZE))
+dtype = torch.FloatTensor
 
-rnn = nn.GRUCell(EMBED_SIZE, CODE_SIZE)
-print(escs[0])
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.reducer = nn.Linear(2 * CODE_SIZE, CODE_SIZE)
+        self.relu = torch.nn.ReLU()
+
+    def reduce(self, left, right):
+        both = torch.cat([left, right], dim=1)
+        reduced = self.reducer(both)
+        return self.relu(reduced)
+
+    def encode(self, batch, lengths):
+        done = [[] for _ in len(batch)]
+        stacks = [[] for _ in len(batch)]
+        for i in range(len(batch[0])):
+            for b in range(len(batch)):
+                # Make sure this is on the right side (ie first elements need to be at end)
+                stacks[b] = stacks[b] + batch[b].pop()
+
+            lefts = [[] for _ in range(len(batch))]
+            rights = [[] for _ in range(len(batch))]
+
+            for b in range(len(batch)):
+                try:
+                    lefts[b] = stacks[b].pop()
+                except IndexError:
+                    # When we're already done with this batch element
+                    lefts[b] = torch.zeros(1, CODE_SIZE).type(dtype)
+                try:
+                    rights[b] = stacks[b].pop()
+                except IndexError:
+                    # When we're already done with this batch element
+                    rights[b] = torch.zeros(1, CODE_SIZE).type(dtype)
+
+            left = torch.cat(lefts, dim=0)
+            right = torch.cat(rights, dim=0)
+            result = self.reduce(left, right)
+            result_split = torch.split(result, 1, dim=0)
+
+            for b in range(len(batch)):
+                stacks[b].push(result_split[b])
+                if b + 1 == lengths[b]:
+                    done[b] = result_split[b]
+        return done
+
+    def forward(self, batch_start_pad, batch_end_pad, lengths):
+        latent_codes = self.encode(batch_start_pad)
+
+
